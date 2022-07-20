0cd811587fb5eae992e6de1c41901d2049fd1fdb
Benson <ben90@gmail.com>
2022-07-20 11:48:03 +0800

    add judge score and text

--- rhythm_game/game.js
+++ rhythm_game/game.js
@@ -1,4 +1,81 @@
-var keys = ['a','s','d','f','h','j','k','l'];
+// note peroidically falls down
+const speed = 1;
+const stop_time = 200; 
+const interval = 100;
+var second = 0;
+var id_num = 0;
+var notes_a = []; // pair element: div + create time
+var notes_s = [];
+
+// time function
+var timeID = window.setInterval(() => {
+    second += 1*speed;
+
+    // add notes every 10 sec
+    var track_a = document.getElementById("track_a");
+    var track_s = document.getElementById("track_s");
+    if((second % 10 == 0) || (second % 15 == 0))
+    {
+        console.log("create new note");
+
+        // add to track a
+        if(second % 10 == 0)
+        {
+            let newNote = document.createElement("div");
+            newNote.classList.add("note");
+            newNote.style.top = '0%';
+            track_a.appendChild(newNote);
+            notes_a.push(newNote);
+            notes_a.push(second);
+        }
+        
+        // add to track s
+        if(second % 15 == 0)
+        {
+            let newNote = document.createElement("div");
+            newNote.classList.add("note");
+            newNote.style.top = '0%';
+            track_s.appendChild(newNote);
+            notes_s.push(newNote);
+            notes_s.push(second);
+        }
+    }
+    
+    // every note drop down by time
+    let i = 0;
+    let all_notes = [notes_a, notes_s];
+    
+    for(let notes of all_notes)
+        for (i=0; i<notes.length; i += 2)
+        {
+            let note = notes[i];
+            let top_int = second - notes[i+1];
+            note.style.top = `${top_int}%` ;
+            // reach judge line
+            if(top_int > 80)
+            {
+                // miss
+                // remove the poped note 
+                (notes.splice(i,1)[0]).remove();
+                notes.splice(i,1);
+                i-=2;
+            }  
+        }
+
+    // stop timer
+    if(second >= stop_time)
+    {
+        window.clearInterval(timeID);
+        console.log("timer stop!");
+    }
+}, interval);      
+
+// available keys
+const keys = ['a','s','d','f','h','j','k','l'];
+// detect if pressed
+var isPressed = {};
+for(let k of keys)
+    isPressed[k] = false;
 
 // light up div when pressed the keyboard
 window.addEventListener("keydown", (e) => {
@@ -11,7 +88,40 @@ window.addEventListener("keydown", (e) => {
         if(!jl.classList.contains("judge_line_pressed"))
             jl.classList.add("judge_line_pressed");
         bt.style.color = 'white';
+
+        // detect perfect, good, bad or miss
+        // detect on the closet note
+        let this_notes = [];
+        if(e.key == 'a')
+            this_notes = notes_a; // pass reference 
+        else if(e.key == 's')
+            this_notes = notes_s;
+        
+        let top_int = second - this_notes[1];
+        
+        if(!isPressed[e.key])
+        {
+            // judged good
+            if(top_int >= 75 && top_int <= 77)
+            {
+                jl.classList.add("judge_line_good");
+                (this_notes.splice(0,1)[0]).remove();
+                this_notes.splice(0,1);
+            }
+            // perfect
+            else if(top_int >= 78 && top_int <= 80)
+            {
+                jl.classList.add("judge_line_perfect");
+                (this_notes.splice(0,1)[0]).remove();
+                this_notes.splice(0,1);
+            }
+        }
     }
+});
+
+// detect if pressed
+window.addEventListener("keypress", (e) =>{
+    isPressed[e.key] = true;
 });
 
 // back off when release keyboard
@@ -20,70 +130,20 @@ window.addEventListener("keyup", (e) => {
     {
         let jl = document.getElementById("jl_" + e.key);
         let bt = document.getElementById("bt_" + e.key);
+        // release key
+        isPressed[e.key] = false;
         
         // if pressed
         if(jl.classList.contains("judge_line_pressed"))
             jl.classList.remove("judge_line_pressed");
         bt.style.color = 'aquamarine';
+
+        // remove score classes
+        if(jl.classList.contains("judge_line_perfect"))
+            jl.classList.remove("judge_line_perfect");
+        if(jl.classList.contains("judge_line_good"))
+            jl.classList.remove("judge_line_good");
+        
     }
 });
-
-// note peroidically falls down
-const speed = 1;
-const stop_time = 200; 
-const interval = 100;
-var second = 0;
-var id_num = 0;
-var notes = document.getElementsByClassName("note");
-var created_times = [];
-//convert to array
-notes = [...notes];
-
-// time function
-var timeID = window.setInterval(() => {
-    second += 1*speed;
-
-    // add notes every 10 sec
-    var track_a = document.getElementById("track_a");
-    var track_s = document.getElementById("track_s");
-    if((second % 10 == 0) || (second % 15 == 0))
-    {
-        console.log("create new note");
-        var newNote = document.createElement("div");
-        newNote.classList.add("note");
-        newNote.style.top = '0%';
-        if(second % 10 == 0)
-            track_a.appendChild(newNote);
-        if(second % 15 == 0)
-            track_s.appendChild(newNote);
-        notes.push(newNote);
-        created_times.push(second);
-    }
-    
-
-    let i = 0;
-    for (let note of notes)
-    {
-        let top_int = parseInt(note.style.top.substr(0,2));
-        top_int = second - created_times[i];
-        note.style.top = `${top_int}%` ;
-        // reach judge line
-        if(top_int >= 80)
-        {
-            notes[i].remove(); //remove this note
-        }  
-        i += 1;
-    }
-
-    // stop timer
-    if(second >= stop_time)
-    {
-        window.clearInterval(timeID);
-        console.log("timer stop!");
-    }
-        
-}, interval);
-
-
-
 
