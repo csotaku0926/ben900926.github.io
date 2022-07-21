ce97e1f94136b4fe9a2dfa99d27e986596730ace
Benson <ben90@gmail.com>
2022-07-21 21:04:37 +0800

    add a start button to start the game

--- rhythm_game/script/game.js
+++ rhythm_game/script/game.js
@@ -71,103 +71,109 @@ function update_score(score){
 // this note should appear in track in time
 var first_note = [0,0,0,0,0,0,0,0];
 
-// play audio after first note is ready
-let audio = document.getElementById("selected_song");
-window.setTimeout(() => {
-    audio.play();
-}, interval * 80 / speed);
+// add the button interaction to play audio
+let confirm_button = document.getElementById("confirm_button");
+confirm_button.addEventListener('click',(() => {
+    console.log("clicked!");
+    confirm_button.style.display = "none";
 
-//fetch notes.json
-fetch("notes.json")
-.then((res) => {
-    return res.json();
-})
-.then((json) => {
-    /* make total score 1000000
-    let m = 0, len = json.length;
-    for(let j=0; j<(len/10);j++)
-        m += (1+j*0.1)*10;
-    m += (1+parseInt(len/10)*0.1)*(len%10);
-    perfect_score = total_score / m;*/
-    // put note into each list with json file
-    for(let note of json["notes"])
-    {
-        let key = keys_dict[note["track"]];
-        // audio may have delay
-        (all_second[key]).push(note["second"] + json["delay"]);
-    }
-    stop_time = json["stop_time"];
-    // time function
-    var timeID = window.setInterval(() => {
-        second += 1*speed;
-        
-        // check each track
-        for(let i=0; i<keys.length; i+=1)
+    // play audio after first note is ready
+    let audio = document.getElementById("selected_song");
+    window.setTimeout(() => {
+        audio.play();
+    }, interval * 80 / speed);
+
+    // fetch notes.json
+    fetch("notes.json")
+    .then((res) => {
+        return res.json();
+    })
+    .then((json) => {
+        /* make total score 1000000
+        let m = 0, len = json.length;
+        for(let j=0; j<(len/10);j++)
+            m += (1+j*0.1)*10;
+        m += (1+parseInt(len/10)*0.1)*(len%10);
+        perfect_score = total_score / m;*/
+        // put note into each list with json file
+        for(let note of json["notes"])
         {
-            let this_first_note = first_note[i];
-            let this_second = all_second[i];
-            // reach time
-            if(this_first_note < this_second.length && this_second[this_first_note] == second)
-            {
-                let this_track = tracks[i];
-                // add note to track
-                let newNote = document.createElement("div");
-                newNote.classList.add("note");
-                newNote.style.top = '0%';
-                this_track.appendChild(newNote);
-                all_notes[i].push(newNote);
-                first_note[i] += 1;
-            }
-
-        }          
-        // every note drop down by time
-        let i = 0;
-
-        for(let n=0; n<all_notes.length; n++)
-            for (i=0; i<all_notes[n].length; i += 1)
-            {
-                let notes = all_notes[n];
-                let seconds = all_second[n];
-                let note = notes[i];
-                let top_int = second - seconds[i];
-                note.style.top = `${top_int}%` ;
-                // reach judge line
-                if(top_int > 80)
-                {
-                    // miss
-                    // update combo
-                    combo_num = 0;
-                    combo_num_span.innerText = `${combo_num}`;
-                    combo_num_span.style.color = "white";
-
-                    let text = document.getElementById(`score_${keys[n]}`);
-                    // add miss text
-                    text.innerText = "MISS";
-                    text.classList.add("miss");
-
-                    // clear effect after a period
-                    window.setTimeout(()=>{
-                        text.classList.remove("miss");
-                    }, 500/speed);
-                    // remove the poped note 
-                    (notes.splice(i,1)[0]).remove();
-                    seconds.splice(i,1);
-                    i-=1;
-                    first_note[n] -=1;
-                }  
-            }
-
-        // stop timer
-        if(second >= stop_time)
-        {
-            window.clearInterval(timeID);
-            audio.pause();
-            audio.currentTime = 0;
+            let key = keys_dict[note["track"]];
+            // audio may have delay
+            (all_second[key]).push(note["second"] + json["delay"]);
         }
-    }, interval);      
+        stop_time = json["stop_time"];
+        // time function
+        var timeID = window.setInterval(() => {
+            second += 1*speed;
+            
+            // check each track
+            for(let i=0; i<keys.length; i+=1)
+            {
+                let this_first_note = first_note[i];
+                let this_second = all_second[i];
+                // reach time
+                if(this_first_note < this_second.length && this_second[this_first_note] == second)
+                {
+                    let this_track = tracks[i];
+                    // add note to track
+                    let newNote = document.createElement("div");
+                    newNote.classList.add("note");
+                    newNote.style.top = '0%';
+                    this_track.appendChild(newNote);
+                    all_notes[i].push(newNote);
+                    first_note[i] += 1;
+                }
 
-});
+            }          
+            // every note drop down by time
+            let i = 0;
 
+            for(let n=0; n<all_notes.length; n++)
+                for (i=0; i<all_notes[n].length; i += 1)
+                {
+                    let notes = all_notes[n];
+                    let seconds = all_second[n];
+                    let note = notes[i];
+                    let top_int = second - seconds[i];
+                    note.style.top = `${top_int}%` ;
+                    // reach judge line
+                    if(top_int > 80)
+                    {
+                        // miss
+                        // update combo
+                        combo_num = 0;
+                        combo_num_span.innerText = `${combo_num}`;
+                        combo_num_span.style.color = "white";
+
+                        let text = document.getElementById(`score_${keys[n]}`);
+                        // add miss text
+                        text.innerText = "MISS";
+                        text.classList.add("miss");
+
+                        // clear effect after a period
+                        window.setTimeout(()=>{
+                            text.classList.remove("miss");
+                        }, 500/speed);
+                        // remove the poped note 
+                        (notes.splice(i,1)[0]).remove();
+                        seconds.splice(i,1);
+                        i-=1;
+                        first_note[n] -=1;
+                    }  
+                }
+
+            // stop timer
+            if(second >= stop_time)
+            {
+                window.clearInterval(timeID);
+                audio.pause();
+                audio.currentTime = 0;
+            }
+        }, interval);      
+
+    });
+}));
 // detect if pressed
 var isPressed = {};
 for(let k of keys)
